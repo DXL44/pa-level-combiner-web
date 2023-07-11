2f1169c65804c42d8770fb045268e030877d28df
DestroyerXL44 <28364459+DXL44@users.noreply.github.com>
2023-07-11 12:44:39 -0400

    Initial commit?

---
+++ README.md
@@ -0,0 +1,7 @@
+# pa-level-combiner-web
+ Website that lets you upload multiple Project Arrhythmia level files (.lsb) and mash them together for collabs.
+
+# What is this?
+This is a web tool that allows you to combine multiple Project Arrhythmia custom level files together into a single file. It's useful for collabs - where it can allow people to work independently without waiting for members to finish. 
+
+It's inspired mostly by [TNTz's Python level combiner](https://github.com/TNTzx/PA-Level-Combiner/), and aims to do what that does but in a webpage format which is a bit more accessible or easier to use for some. 
---
+++ main.js
@@ -0,0 +1,75 @@
+	/* all of the code here is probably really bad i'm sorry lmao */
+// ALRIGHT NERDS. its time to put together the level file
+
+//Its time to update the colors but be WAY COOLER
+//COLOR IDS will be in this format: [group][number]. 
+//COLOR GROUPS: gui, bg, players, objs, bgs (aka the groups in the uh... uh the )
+
+
+//make the level! woohoo!    
+//document.getElementById("EditorStuff").style.display = "block";
+//const inputThings = document.getElementsByClassName("hideOnLoad");
+//for (let i = 0; i < inputThings.length; i++) {
+//  inputThings[i].style.display = "none";
+//}
+let theRealLevel = null
+let levelJSON = null
+
+function storeLevel(){
+    input = document.getElementById('levelupload');
+    let file = input.files[0]; 
+    let fileReader = new FileReader(); 
+    fileReader.readAsText(file); 
+    fileReader.onload = function() {
+        theRealLevel = fileReader.result
+        }; 
+    fileReader.onerror = function() {
+          alert(fileReader.error);
+    }
+}
+function loadLevel() {
+    inputLevel = theRealLevel
+    if (inputLevel == ""){
+        console.log("oops don't do that")
+        return
+    } else {
+        console.log("doing it...");
+        levelJSON = JSON.parse(inputLevel);
+        console.log("LOADED LEVEL JSON");
+        statOutput = document.getElementById("infotest")
+        statOutput.innerHTML += `<br>INITIAL MARKERS: ${Object.keys(levelJSON.ed.markers).length}`
+        console.log("HOLY SHIT THE SECOND MARKER IN THIS LEVEL IS " + levelJSON.ed.markers[2] + "!!!!")
+        //combine the markers
+        // I REALLY WISH I WERENT ITERATING RIGHT NOW
+        let itemLength
+        itemLength = levelJSON.ed.markers.length
+        for (let i = 0; i < (itemLength-1); i++) {
+            console.log("Adding item" + levelJSON.ed.markers[i])
+            levelJSON.ed.markers.push(levelJSON.ed.markers[i])
+        
+        }
+        levelJSON.ed.markers.push(levelJSON.ed.markers)
+        console.log('lmao done')
+        console.log(levelJSON.ed.markers) 
+        console.log("LOADING THE LEVEL STATS YIPPIE!!!!!!!!!")
+        statOutput.innerHTML += `<br>OHHHH SHIT I DOUBLED THEM NOW ITS: ${Object.keys(levelJSON.ed.markers).length}`
+    }
+}
+
+
+
+// download the level
+function download(filename, text){
+    var element = document.createElement('a');
+    element.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download',filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function saveLevel() {
+    var finalLevel = JSON.stringify(levelJSON)
+    download("level"+ ".lsb", finalLevel);
+}
